7fdc77f555199de62f808ea31121439e569c328f
vivekpmenon013 <vivekpmenon013@gmail.com>
2022-10-10 18:42:30 +0530

    Committed Changes related to Task 20

---
+++ Tasks/CustomValidators/ValidateDateRange.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Tasks.CustomValidators
+{
+    public class ValidateDateRange : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            // your validation logic
+            if (Convert.ToDateTime(value) >= Convert.ToDateTime("01/01/1900") && Convert.ToDateTime(value) <= Convert.ToDateTime("01/01/2090"))
+            {
+                return ValidationResult.Success;
+            }
+            else
+            {
+                return new ValidationResult("Date should be between 1/1/1900 and 1/1/2090");
+            }
+        }
+    }
+}
