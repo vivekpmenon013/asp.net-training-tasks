ee7b71dedc4017647ad4b787a4ad703634055d1a
vivekpmenon013 <vivekpmenon013@gmail.com>
2022-10-12 18:05:23 +0530

    Completed Task 22

---
+++ Tasks/Controllers/DigitsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tasks.Models;
+
+namespace Tasks.Controllers
+{
+    public class DigitsController : Controller
+    {
+        // GET: Digits
+        public ActionResult Index()
+        {
+            ViewBag.Task = "Task 22 - Find Unique Digits in 9 digit number";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(DigitViewModel dvm)
+        {
+            ViewBag.Task = "Task 22 - Find Unique Digits in 9 digit number";
+            var list = dvm.Number.ToString().Select(c => int.Parse(c.ToString())).ToList();
+            List<int> myList = list.Distinct().ToList();
+            dvm.UniqueDigits = string.Join("", myList);
+            return View(dvm);
+        }
+    }
+}
---
+++ Tasks/Models/DigitViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Tasks.Models
+{
+    public class DigitViewModel
+    {
+        [Required]
+        [Display(Description ="Please enter a 9 digit number")]
+        [RegularExpression(@"^[0-9]{9}$", ErrorMessage = "Please enter 9 digit number")]
+        public long Number { get; set; }
+
+        [Display(Description = "Unique Digits in the entered number are below")]
+        public string UniqueDigits { get; set; }
+    }
+}
