f326e804924af6db3594a589f74112f8c14a9bec
vivekpmenon013 <vivekpmenon013@gmail.com>
2022-10-12 10:28:23 +0530

    Completed Task 21 and added required files

---
+++ Tasks/Controllers/FlowerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tasks.Models;
+
+namespace Tasks.Controllers
+{
+    public class FlowerController : Controller
+    {
+        // GET: Flower
+        public ActionResult Index()
+        {
+            ViewBag.Task = "Task 21 - RadioList";
+            FlowerViewModel fvm = new FlowerViewModel();
+            return View(fvm);
+        }
+
+        [HttpPost]
+        public ActionResult Index(FlowerViewModel fvm)
+        {
+            ViewBag.Task = "Task 21 - RadioList";
+            return View(fvm);
+        }
+    }
+}
